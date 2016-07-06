3bd17479c02608af0a705716030a73a9c11c9bae
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-07-06 10:20:10 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,40 +11,23 @@ r = RandomPerson()
 params = {}
 
 ######genrates random athletes
-# nations = Nation.all()
-# nations.each do |nation|
-#   rand(25..45).times do
-#   person = r.generate()
-#   params['name'] = "#{person.first} #{person.last}"
-#   params['dob'] = person.dob
-#   if person.gender = "m"
-#     params['sex'] = "male"
-#   else
-#     params['sex'] = "female"
-#   end
-#   params['height'] = rand(150..200)
-#   params['weight'] = rand(60..120)
-#   params['nation_id'] = nation.id
-#   athlete = Athlete.new(params).save
-#   end
-# end
-
-events = Event.all()
-
-events.each do |event|
-
-  if event.participation_type == "athlete"
-
+nations = Nation.all()
+nations.each do |nation|
+  rand(3..10).times do
+  person = r.generate()
+  params['name'] = "#{person.first} #{person.last}"
+  params['dob'] = person.dob
+  if person.gender = "m"
+    params['sex'] = "male"
   else
-
-
-
-
-
-
-
+    params['sex'] = "female"
+  end
+  params['height'] = rand(150..200)
+  params['weight'] = rand(60..120)
+  params['nation_id'] = nation.id
+  athlete = Athlete.new(params).save
+  end
 end
-
 
 
 
